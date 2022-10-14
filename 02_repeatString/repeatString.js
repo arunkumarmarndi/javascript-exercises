077214316c5e8f19c32e38f1545cdd510bf21017
Arun Kumar Marndi <arunkumarmarndi1994@gmail.com>
2022-10-14 14:49:31 +1100

    02_repeatString and 03_reverseString solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(a,b) {
+    if(b < 0){
+        return "ERROR";
+    }
+    return a.repeat(b);
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(a) {
+    var newString = "";
+    if(a.length === 0){
+        return "";
+    }
+    for (var i = a.length - 1; i >= 0; i--) {
+        newString += a[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
