02652faaeb00348beac79d6e74d0c29113810ec4
Arun Kumar Marndi <arunkumarmarndi1994@gmail.com>
2022-10-19 21:28:01 +1100

    04_removeFromArray exercise complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    if(arr.length == 0){
+        return [];
+    }
+    else{
+        for(let i=1;i<arguments.length;i++){
+            let value = arguments[i];
+            arr = arr.filter(function(item) {
+                return item !== value;
+            })
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
