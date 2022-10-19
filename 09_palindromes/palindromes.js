f41460ab91ddb26e4b48040049e364e06257dec2
Arun Kumar Marndi <arunkumarmarndi1994@gmail.com>
2022-10-20 01:25:37 +1100

    09_palindrome exercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (the_string) {
+    the_string = the_string.toLowerCase();
+    let reg = /[\s-!$%^&*()_+|~=`{}\[\]:\/;<>?,.@#]/g
+    let filtered_string = the_string.replace(reg,"");
+    let reversed_string = filtered_string.split('').reverse().join('');
+    if(filtered_string === reversed_string){
+        return true;
+    }
+    else{
+        return false;
+    }
 
 };
 
