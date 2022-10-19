11172894fc771b2f2fef223b5b3fb32d3aa0b3bc
Arun Kumar Marndi <arunkumarmarndi1994@gmail.com>
2022-10-20 00:29:56 +1100

    08_calculator exercise finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  total = 0;
+	for(let i=0;i<arr.length;i++){
+    total = total + arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  product = 1;
+	for(let i=0;i<arr.length;i++){
+    product = product * arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n === 1){
+    return 1;
+  }
+  else{
+    let fact = 1;
+    for(let i=1;i<=n;i++){
+      fact = fact * i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
