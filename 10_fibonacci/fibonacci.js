df7ea1dd474bbc1ddd1f83a10cc1c51c83be3aa6
Arun Kumar Marndi <arunkumarmarndi1994@gmail.com>
2022-10-20 01:51:08 +1100

    10_fibonacci exercise solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n instanceof String){
+        n = parseInt(n);
+    }
+    if(n<0){
+        return "OOPS";
+    }
+    if(n===0){
+        return 0;
+    }
+    else{
+        let a = 0;
+        let b = 1;
+        let s = 0;
+        if(n<=2){
+            return 1;
+        }
+        else{
+            for(let i=2;i<=n;i++){
+            s = a + b;
+            a = b;
+            b = s;
+            }
+        return b;
+    }
+    }
 };
 
 // Do not edit below this line
