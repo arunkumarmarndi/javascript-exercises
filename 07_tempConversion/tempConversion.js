cab2fb6fc06534f7a5a7daa18679bb863568e4c6
Arun Kumar Marndi <arunkumarmarndi1994@gmail.com>
2022-10-19 23:37:12 +1100

    07_tempConversions exercise complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = 5/9*(f-32);
+  c = parseFloat(c.toFixed(1));
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (9/5*c) + 32;
+  f = parseFloat(f.toFixed(1));
+  return f;
 };
 
 // Do not edit below this line
